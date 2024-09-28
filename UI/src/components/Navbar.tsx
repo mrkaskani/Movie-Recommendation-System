368a0fc306902b31f8f539da25a7fb696f495e3c
m.kaskani <mrkaskani103@gmail.com>
2024-09-28 22:02:38 +0330

    init commit

---
+++ UI/src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+export function Navbar() {
+return (
+  <>
+    <header className={"header"}>
+      <nav className={"navbar"}>
+        <ul className={"nav"}>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Movies</a></li>
+          <li><a href="#">Reviews</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+      </nav>
+    </header>
+  </>
+)
+}
